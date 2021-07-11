f054970438a21b55759400ca306801d463e326da
Vinh Huynh <caubequay00@gmail.com>
2021-07-11 17:46:01 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,6 +3,6 @@ if [ "$TARGETARCH" = "amd64" ] ; then ARCH=linux;fi
 if [ "$TARGETARCH" = "arm64" ] ; then ARCH=aarch64;fi
 if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
 VER=$1
-_VER=echo $1 |cut -d "-" -f 1 | cut -d "v" -f 2
+_VER=$(echo $VER |cut -d "-" -f 1 | cut -d "v" -f 2)
 echo "https://github.com/hpool-dev/chia-miner/releases/download/${_VER}/HPool-Miner-chia-${VER}-${ARCH}.zip "
 wget -q --no-check-certificate https://github.com/hpool-dev/chia-miner/releases/download/${_VER}/HPool-Miner-chia-${VER}-${ARCH}.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
